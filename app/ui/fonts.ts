ec056d01903ebfc4a74e94d98b4cbb39a332ab16
annabelen.ra@gmail.com <annabelen.ra@gmail.com>
2023-12-12 13:38:40 -0300

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Lusitana, Montserrat } from 'next/font/google';
+import {Inter } from 'next/font/google';
+export const monserrat=Montserrat({subsets: ['latin']})
+
+export const inter=Inter({subsets: ['latin']})
+
+export const lusitana=Lusitana({subsets: ['latin'], weight: ['400','700']})
